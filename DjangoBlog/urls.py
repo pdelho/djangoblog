4d5e7ad7814d94960b9780817b38e219ea241cfd
pablo <pablodelhoyo@gmail.com>
2019-07-21 20:08:15 +0200

    Get all posts and get one post. Mapping urls and loading views

--- DjangoBlog/urls.py
+++ DjangoBlog/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog.views import home, post
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/<postId>', post),
+    path('', home),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def home(request):
+    posts = Post.objects.all()
+    return render(request, "blog/home.html", {'posts':posts})
+
+def post(request, postId):
+    post = Post.objects.get(id=postId)
+    return render(request, "blog/post.html", {'post':post})
